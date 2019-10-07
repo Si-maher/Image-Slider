2ba019b7bb37efa896e60c62e9c2748b624297e4
Simon Maher <simonmaher8307@hotmail.com>
2019-10-07 11:44:17 +0300

    created function for nextBtn slide

--- index.js
+++ index.js
@@ -2,7 +2,17 @@ const prevBtn = document.querySelector('.prevBtn');
 const nextBtn = document.querySelector('.nextBtn');
 const container = document.querySelector('.images')
 
-let counter = 0;
 
-prevBtn.addEventListener('click', nextSlide)
-nextBtn.addEventListener('click', prevSlide)
+let counter = 0;
+nextBtn.addEventListener('click', nextSlide)
+prevBtn.addEventListener('click', prevSlide)
+
+
+function nextSlide(){
+    if (counter === 4) {
+        counter= -1;
+    }
+    counter++
+    
+    container.style.backgroundImage = `url(img/bcg-${counter}.jpeg)`
+}
